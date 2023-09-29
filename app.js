e673dfe424eb21eb6fc5f3bc667e3d8594df9eed
Serkan.durgut <Serkan.durgut@mobilpark.biz>
2023-09-29 06:48:28 +0300

    convert multithread

--- app.js
+++ app.js
@@ -9,46 +9,116 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
     });
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const fs = require('fs');
 const httpClientService_1 = require("./Services/httpClientService");
+const { Worker, isMainThread, parentPort, workerData } = require('worker_threads');
+const fs = require('fs');
+const readline = require('readline');
+const os = require('os');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+const outputPath = 'output.txt';
+const numCores = os.cpus().length;
+const requestParameter = {
+    fullEndPoint: 'https://raw.githubusercontent.com/bilalozdemir/tr-word-list/master/files/words.json',
+    headers: {
+        'Content-Type': 'application/json',
+    },
+};
 function main() {
     return __awaiter(this, void 0, void 0, function* () {
-        const https = require('https');
-        const readline = require('readline');
-        const rl = readline.createInterface({
-            input: process.stdin,
-            output: process.stdout
-        });
-        // Uygulama kodunuz
-        const requestParameter = {
-            fullEndPoint: 'https://raw.githubusercontent.com/bilalozdemir/tr-word-list/master/files/words.json',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-        };
-        const wordsFromApiArray = [];
-        var client = new httpClientService_1.httpsClientService();
-        var response = yield client.get({ requestParameter });
-        response.forEach((item) => {
-            wordsFromApiArray.push(item.word);
-        });
-        while (true) {
-            const randomIndex = Math.floor(Math.random() * wordsFromApiArray.length);
-            const selectedWord = wordsFromApiArray[randomIndex];
-            fs.appendFileSync('ApiWords2.txt', selectedWord + '\n', 'utf8');
-            const path = 'ApiWords2.txt';
-            const stats = fs.statSync(path);
-            const fileSize = stats.size;
-            const MB = fileSize / (1024 * 1024);
-            if (MB >= 2) {
-                console.log('Dosya 2 MB veya daha b�y�k.');
-                break; // D�ng�y� burada sonland�r�n
-            }
-        }
-        rl.question('Uygulama sonland�. ��kmak i�in Enter tu�una bas�n...', () => {
-            rl.close();
-        });
+        const client = new httpClientService_1.httpsClientService();
+        const response = yield client.get({ requestParameter });
+        const wordsFromApiArray = response.map(item => item.word);
+        const filteredWords = wordsFromApiArray.filter(word => word.length >= 4);
+        const uniqueWords = Array.from(new Set(filteredWords));
+        return uniqueWords;
     });
 }
-main();
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+const getFileSize = (path) => {
+    try {
+        const stats = fs.statSync(path);
+        return stats.size; // Dosya boyutunu d�nd�r
+    }
+    catch (error) {
+        console.error('Dosya boyutu �l�me hatas�:', error);
+        return -1; // Hata durumunda -1 d�nd�r
+    }
+};
+const doesFileExist = (path) => {
+    try {
+        fs.accessSync(path); // E�er dosya varsa bu sat�r hata vermez
+        return true; // Dosya var
+    }
+    catch (error) {
+        return false; // Dosya yok
+    }
+};
+const getSelectRandomWords = (words) => {
+    try {
+        const randomWord = words[Math.floor(Math.random() * words.length)];
+        return randomWord || '';
+    }
+    catch (error) {
+        console.error('getSelectRandomWords error:', error);
+        return '';
+    }
+};
+function run() {
+    return __awaiter(this, void 0, void 0, function* () {
+        const words = yield main();
+        const outputFile = "output.txt";
+        const startTime = Date.now();
+        yield sleep(100);
+        let fileSize = 0;
+        if (isMainThread) {
+            console.log(`Ana i� par�ac��� ba�lad�: ${new Date().toISOString()}`);
+            const worker = new Worker(__filename, { workerData: { outputPath: outputFile, words } });
+            // Yard�mc� i� par�ac���ndan mesaj al�nd���nda
+            worker.on('message', data => {
+                if (data != "completed") {
+                    console.log(`Dosya Boyutu: ${data}`);
+                }
+                else {
+                }
+            });
+            // Yard�mc� i� par�ac���ndan hata al�nd���nda
+            worker.on('error', error => {
+                console.error(`Ana i� par�ac���: Yard�mc� i� par�ac��� hatas�: ${error.message}`);
+            });
+            // Yard�mc� i� par�ac���ndan ��k�� al�nd���nda
+            worker.on('exit', exitCode => {
+                console.log(`Ana i�  parcacigi bitti  `);
+            });
+        }
+        else {
+            const { outputPath, words } = workerData || {};
+            if (outputPath) {
+                let totalData = '';
+                while (fileSize < 4 * 1024 * 1024 * 1024) {
+                    const randomWord = getSelectRandomWords(words);
+                    // Veriyi biriktir
+                    totalData += randomWord + '\n';
+                    fs.writeFileSync(outputFile, totalData, { encoding: 'utf8', flag: 'a+' });
+                    if (doesFileExist(outputFile)) {
+                        fileSize = getFileSize(outputFile);
+                        if (fileSize !== -1) {
+                            parentPort.postMessage(fileSize);
+                        }
+                    }
+                }
+                // Yard�mc� i� par�ac���ndan ��k�� yap
+                process.exit(0);
+            }
+            else {
+                console.error('workerData i�inde start ve end bulunamad�.');
+            }
+        }
+    });
+}
+run();
 //# sourceMappingURL=app.js.map
--- app.ts
+++ app.ts
@@ -1,60 +1,160 @@
-
-const fs = require('fs');
-import { waitForDebugger } from "inspector";
 import { WordJsonModel } from "./Models/WordJsonModel";
 import { httpsClientService, RequestParameters } from "./Services/httpClientService";
-import { WordsFromApi } from "./Models/Words";
+const { Worker, isMainThread, parentPort, workerData } = require('worker_threads');
+const fs = require('fs');
+
+const readline = require('readline');
+const os = require('os');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+const outputPath = 'output.txt';
+const numCores = os.cpus().length;
+
+const requestParameter: RequestParameters = {
+    fullEndPoint: 'https://raw.githubusercontent.com/bilalozdemir/tr-word-list/master/files/words.json',
+    headers: {
+        'Content-Type': 'application/json',
+    },
+};
+
+
 async function main() {
+    const client = new httpsClientService();
+    const response = await client.get<WordJsonModel[]>({ requestParameter });
 
-    const https = require('https');
+    const wordsFromApiArray = response.map(item => item.word);
+    const filteredWords = wordsFromApiArray.filter(word => word.length >= 4);
+    const uniqueWords = Array.from(new Set(filteredWords));
 
-    const readline = require('readline');
+    return uniqueWords;
+}
 
-    const rl = readline.createInterface({
-        input: process.stdin,
-        output: process.stdout
-    });
-    // Uygulama kodunuz
-    const requestParameter: RequestParameters = {
-        fullEndPoint: 'https://raw.githubusercontent.com/bilalozdemir/tr-word-list/master/files/words.json',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-    };
-        const wordsFromApiArray = [];
-    var client = new httpsClientService(); 
-    var response = await client.get<WordJsonModel[]>({ requestParameter });
-     
-    response.forEach((item) => {
-         wordsFromApiArray.push(item.word)
-    });
 
-    while (true) {
-        const randomIndex = Math.floor(Math.random() * wordsFromApiArray.length);
-        const selectedWord = wordsFromApiArray[randomIndex];
-        fs.appendFileSync('ApiWords2.txt', selectedWord + '\n', 'utf8');
 
-        const path = 'ApiWords2.txt';
-        const stats = fs.statSync(path);  
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
 
-        const fileSize = stats.size;
-        const MB = fileSize / (1024 * 1024 );
 
-        
 
-        if (MB >= 2) {
-            console.log('Dosya 2 MB veya daha büyük.');
-            break; // Döngüyü burada sonlandýrýn
+
+const getFileSize = (path: string): number => {
+    try {
+        const stats = fs.statSync(path);
+        return stats.size; // Dosya boyutunu döndür
+    } catch (error) {
+        console.error('Dosya boyutu ölçme hatasý:', error);
+        return -1; // Hata durumunda -1 döndür
+    }
+};
+
+
+
+const doesFileExist = (path: string): boolean => {
+    try {
+        fs.accessSync(path); // Eðer dosya varsa bu satýr hata vermez
+        return true; // Dosya var
+    } catch (error) {
+        return false; // Dosya yok
+    }
+};
+
+const getSelectRandomWords = (words: string[]) => {
+    try {
+        const randomWord = words[Math.floor(Math.random() * words.length)];
+        return randomWord || '';
+    } catch (error) {
+        console.error('getSelectRandomWords error:', error);
+        return '';
+    }
+
+};
+async function run() {
+    const words = await main();
+    const outputFile: string = "output.txt";
+    const startTime = Date.now();
+    await sleep(100);
+    let fileSize = 0;
+
+    if (isMainThread) {
+        console.log(`Ana iþ parçacýðý baþladý: ${new Date().toISOString()}`);
+
+
+        const worker = new Worker(__filename, { workerData: { outputPath: outputFile, words } });
+
+
+
+
+        // Yardýmcý iþ parçacýðýndan mesaj alýndýðýnda
+        worker.on('message', data => {
+            if (data != "completed") {
+                console.log(`Dosya Boyutu: ${data}`);
+            } else {
+
+            }
+
+        });
+
+
+        // Yardýmcý iþ parçacýðýndan hata alýndýðýnda
+        worker.on('error', error => {
+            console.error(`Ana iþ parçacýðý: Yardýmcý iþ parçacýðý hatasý: ${error.message}`);
+        });
+
+
+        // Yardýmcý iþ parçacýðýndan çýkýþ alýndýðýnda
+        worker.on('exit', exitCode => {
+            console.log(`Ana iþ  parcacigi bitti  `);
+        });
+
+
+    }
+    else {
+        const { outputPath, words } = workerData || {};
+
+
+
+
+        if (outputPath) {
+            let totalData = '';
+
+
+            while (fileSize < 4 * 1024 * 1024 * 1024) {
+                const randomWord = getSelectRandomWords(words);
+                // Veriyi biriktir
+                totalData += randomWord + '\n';
+                fs.writeFileSync(outputFile, totalData, { encoding: 'utf8', flag: 'a+' });
+
+                if (doesFileExist(outputFile)) {
+                    fileSize = getFileSize(outputFile);
+
+                    if (fileSize !== -1) {
+                        parentPort.postMessage(fileSize);
+                    }
+
+                }
+            }
+
+            // Yardýmcý iþ parçacýðýndan çýkýþ yap
+            process.exit(0);
+
+
         }
+        else {
+            console.error('workerData içinde start ve end bulunamadý.');
+        }
+
+
+
     }
 
 
 
-   
- 
-    rl.question('Uygulama sonlandý. Çýkmak için Enter tuþuna basýn...', () => {
-        rl.close();
-    });
-            
 }
-main();
+
+run();
+
